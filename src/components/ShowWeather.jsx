70e991c537a4502a0f7f9196bd572289881c4efe
Daniel Zamudio Alonso <danzamudio1@hotmail.com>
2022-05-03 00:49:49 -0500

    Terminado

--- src/components/ShowWeather.jsx
+++ src/components/ShowWeather.jsx
@@ -1,13 +1,27 @@
 import React from "react";
 
 import { makeStyles } from '@material-ui/core/styles';
-import { Card, CardContent, CardMedia, Grid } from "@material-ui/core";
+import { Card, CardContent, CardMedia, Grid, Paper } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
     root: {
         paddingTop: '1%',
     },
+    card: {
+        display: 'block',
+        height: '50%',
+    },
+    paper: {
+        padding: theme.spacing(1),
+        textAlign: 'center',
+    },
 }));
+
+const cardStyle = {
+    display: "block",
+    transitionDuration: "0.3s",
+    height: "30vw"
+};
 
 const ShowWeather = (data) => {
     const dataWeather = data.data;
@@ -16,11 +30,14 @@ const ShowWeather = (data) => {
 
     const classes = useStyles();
     let imgurl = '';
+    let iconurl = '';
 
-    dataWeather.data ? 
-        imgurl = `../img/${dataWeather.data.weather[0].main}.jpg`
-    : imgurl = '/img/clima.jpg'
-
+    if (dataWeather.data) {
+        iconurl = `http://openweathermap.org/img/wn/${dataWeather.data.weather[0].icon}.png`;
+        imgurl = `../img/${dataWeather.data.weather[0].main}.jpg`;
+    } else {
+        imgurl = '/img/clima.jpg'
+    }
 
     return (
 
@@ -34,13 +51,42 @@ const ShowWeather = (data) => {
             >
                 <Grid item >
                     <Card>
-                        <CardMedia
-                            component="img"
-                            src={imgurl}
-                            title="Image with weather"
-                        />
+                        <div style={cardStyle}>
+                            <CardMedia
+                                component="img"
+                                src={imgurl}
+                                title="Image with weather"
+                            />
+                        </div>
                         <CardContent>
-                            contenido
+                            {
+                                dataWeather.data ? (
+                                    <div>
+                                        <Paper className={classes.paper}>
+                                        <img src={iconurl} alt="" srcset="" />
+                                        </Paper>
+                                        <Grid container item xs={12} spacing={3}>
+                                            <Grid item xs={4}>
+                                                <Paper className={classes.paper}>
+                                                    Temperatura: {Math.floor(dataWeather.data.main.temp - 273.15)}°
+                                                </Paper>
+                                            </Grid>
+                                            <Grid item xs={4}>
+                                                <Paper className={classes.paper}>
+                                                    Descripción: {dataWeather.data.weather[0].description}
+                                                </Paper>
+                                            </Grid>
+                                            <Grid item xs={4}>
+                                                <Paper className={classes.paper}>
+                                                    Maxima: {Math.floor(dataWeather.data.main.temp_max - 273.15)}°
+                                                    {'\n'}
+                                                    Minima: {Math.floor(dataWeather.data.main.temp_min - 273.15)}°
+                                                </Paper>
+                                            </Grid>
+                                        </Grid>
+                                    </div>
+                                ) : ''
+                            }
                         </CardContent>
                     </Card>
                 </Grid>
